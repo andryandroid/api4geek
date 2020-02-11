2a08782facf27045adfc3e9808b5d0a2c6769f87
roanagab <roanagab@gmail.com>
2020-02-11 16:09:14 -0300

    rodrigo

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, request, jsonify, render_template
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from models import db, Contact
+from models import db, Contact, Usuario, Evento, Requerimiento, Item, Imagen, Participante
 
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
@@ -32,7 +32,7 @@ def contacts(id=None):
         if id is not None:
             contact = Contact.query.get(id)
             if contact:
-                return jsonify(Contact.serialize()), 200
+                return jsonify(contact.serialize()), 200
             else:
                 return jsonify({"msg":"Contact not found"}), 404
         else:
@@ -94,6 +94,128 @@ def contacts(id=None):
 
         return jsonify({"msg":"Contact deleted"}), 200
 
+
+@app.route("/usuario", methods=['GET', 'POST'])
+@app.route("/usuario/<int:id>", methods=['GET', 'PUT', 'DELETE'])
+
+def usuario(id=None):
+    if request.method == 'GET':
+        if id is not None:
+            usuario = Usuario.query.get(id)
+            if usuario:
+                return jsonify(usuario.serialize()), 200
+            else:
+                return jsonify({"msg":"User not found"}), 404
+        else:
+            usuario = Usuario.query.all()
+            usuario = list(map(lambda usuario: usuario.serialize(), usuario))
+            return jsonify(usuario), 200
+
+    if request.method == 'POST':
+        nombre = request.json.get('nombre', None)
+        apellido = request.json.get('apellido', None)
+        correo = request.json.get('correo', None)
+        ubicacion = request.json.get('ubicacion', None)
+        descripcion = request.json.get('descripcion', None)
+        contrasena = request.json.get('contrasena', None)
+        nombre_usuario = request.json.get('nombre_usuario', None)
+        imagen_perfil = request.json.get('imagen_perfil', None)
+
+        if not nombre:
+            return jsonify({"msg":"name is required"}), 422
+
+        if not apellido:
+            return jsonify({"msg":"Last name is required"}), 422
+
+        if not correo:
+            return jsonify({"msg":"email is required"}), 422
+
+        if not contrasena:
+            return jsonify({"msg":"password is required"}), 422
+
+        if not nombre_usuario:
+            return jsonify({"msg":"user name is required"}), 422    
+
+        usuario = Usuario()
+        usuario.nombre = nombre
+        usuario.apellido = apellido
+        usuario.correo = correo
+        usuario.ubicacion = ubicacion
+        usuario.descripcion = descripcion
+        usuario.contrasena = contrasena
+        usuario.nombre_usuario = nombre_usuario
+        usuario.imagen_perfil = imagen_perfil
+
+
+        db.session.add(usuario)
+        db.session.commit()
+
+        return jsonify(usuario.serialize()), 201
+
+
+    if request.method == 'PUT':
+        nombre = request.json.get('nombre', None)
+        apellido = request.json.get('apellido', None)
+        correo = request.json.get('correo', None)
+        ubicacion = request.json.get('ubicacion', None)
+        descripcion = request.json.get('descripcion', None)
+        contrasena = request.json.get('contrasena', None)
+        nombre_usuario = request.json.get('nombre_usuario', None)
+        imagen_perfil = request.json.get('imagen_perfil', None)
+
+        if not nombre:
+            return jsonify({"msg":"name is required"}), 422
+
+        if not apellido:
+            return jsonify({"msg":"Last name is required"}), 422
+
+        if not correo:
+            return jsonify({"msg":"email is required"}), 422
+
+        if not ubicacion:
+            return jsonify({"msg":"location is required"}), 422
+
+        if not descripcion:
+            return jsonify({"msg":"description is required"}), 422
+
+        if not contrasena:
+            return jsonify({"msg":"password is required"}), 422
+
+        if not nombre_usuario:
+            return jsonify({"msg":"user name is required"}), 422    
+        
+        if not imagen_perfil:
+            return jsonify({"msg":"profile image is required"}), 422
+
+        usuario = Usuario.query.get(id)
+
+        if not usuario:
+                return jsonify({"msg":"User not found"}), 404
+
+        usuario.nombre = nombre
+        usuario.apellido = apellido
+        usuario.correo = correo
+        usuario.ubicacion = ubicacion
+        usuario.descripcion = descripcion
+        usuario.contrasena = contrasena
+        usuario.nombre_usuario = nombre_usuario
+        usuario.imagen_perfil = imagen_perfil
+
+        db.session.commit()
+
+        return jsonify(usuario.serialize()), 200
+
+    if request.method == 'DELETE':
+
+        usuario = Usuario.query.get(id)
+
+        if not usuario:
+                return jsonify({"msg":"User not found"}), 404
+
+        db.session.delete(usuario)
+        db.session.commit()
+
+        return jsonify({"msg":"User deleted"}), 200
 
 if __name__=="__main__":
     manager.run()
--- models.py
+++ models.py
@@ -17,3 +17,139 @@ class Contact(db.Model):
             "name": self.name,
             "phone": self.phone
         }
+
+
+class Usuario(db.Model):
+    __tablename__ = 'usuario'
+    id = db.Column ( db.Integer, primary_key=True)
+    nombre = db.Column ( db.String(50), nullable = False)
+    apellido = db.Column ( db.String(50),nullable=False)
+    correo = db.Column ( db.String(50),nullable=False)
+    ubicacion = db.Column (db.String(50))
+    descripcion = db.Column (db.String(200))
+    contrasena = db.Column (db.String(50), nullable=False)
+    nombre_usuario = db.Column (db.String(50), nullable=False)
+    imagen_perfil = db.Column (db.String(100))
+    requerimiento = db.relationship("Association", back_populates="requerimiento")
+        
+    def __repr__(self):
+        return "<Usuario %r>" % self.nombre_usuario
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "correo": self.correo,
+            "ubicacion": self.ubicacion,
+            "descripcion": self.descripcion,
+            "contrasena": self.contrasena,
+            "nombre_usuario": self.nombre_usuario,
+            "imagen_perfil": self.imagen_perfil
+        }
+
+class Requerimiento(db.Model):
+    __tablename__ = 'requerimiento'
+    evento_id = db.Column(db.Integer, db.ForeignKey('evento.id'), primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('item.id'), primary_key=True)
+    cantidad_requerida = db.Column ( db.Integer, nullable = False)
+    cantidad_actual = db.Column ( db.Integer, nullable = False)
+    estado_requerimiento = db.Column (db.String(50))
+    item = db.relationship("Item")
+    usuario = db.relationship("Association", back_populates="usario")
+        
+    def __repr__(self):
+        return "<Requerimiento %r>" % self.cantidad_requerida
+
+    def serialize(self):
+        return {
+            "evento_id": self.evento_id,
+            "item_id": self.item_id,
+            "cantidad_requerida": self.cantidad_requerida,
+            "cantidad_actual": self.cantidad_actual, 
+            "estado del requerimiento": self.estado_requerimientos
+        }
+
+class Evento(db.Model):
+    __tablename__ = 'evento'
+    id = db.Column ( db.Integer, primary_key=True)
+    titulo = db.Column ( db.String(50), nullable = False)
+    descripcion = db.Column ( db.String(200),nullable=False)
+    fecha_limite = db.Column ( db.String(50),nullable=False)
+    estado_evento = db.Column (db.String(50))
+    imagen = db.relationship("Imagen", uselist=False, back_populates="evento")
+    usuario_id = db.Column(db.Integer, db.ForeignKey('usuario.id'))
+    usuario = db.relationship("Usuario")
+    requerimientos = db.relationship("Evento_Requerimiento")  
+   
+    def __repr__(self):
+        return "<Evento %r>" % self.titulo
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "titulo": self.titulo,
+            "descripcion": self.descripcion,
+            "fecha_limite": self.fecha_limite,
+            "estado_evento": self.estado_evento,
+            "usuario_id": self.usuario_id
+        }
+
+class Item(db.Model):
+    __tablename__ = 'item'
+    id = db.Column ( db.Integer, primary_key=True)
+    nombre = db.Column ( db.String(50), nullable = False)
+    descripcion = db.Column ( db.String(50),nullable=False)
+      
+    def __repr__(self):
+        return "<Item %r>" % self.nombre
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "descripcion": self.descripcion,          
+
+        }
+
+class Imagen(db.Model):
+    __tablename__ = 'imagen'
+    id = db.Column ( db.Integer, primary_key=True)
+    evento = db.relationship("Evento", back_populates="imagen")
+    evento_id = db.Column ( db.Integer, db.ForeignKey(evento.id))
+    imagen_Evento = db.Column ( db.String(250),nullable=False)
+     
+
+    def __repr__(self):
+        return "<Imagen %r>" % self.imagen_Evento
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "imagen de evento": self.imagen_Evento,
+            "id de evento": self.evento_id,
+        }
+
+class Participante(db.Model):
+    __tablename__ = 'Participante'
+    id = db.Column ( db.Integer, primary_key=True)
+    item = db.relationship("Item", back_populates="item")
+    evento = db.relationship("Evento", back_populates="evento")
+    usuario = db.relationship("Usuario", back_populates="usuario")
+    item_id = db.Column ( db.Integer, db.ForeignKey(item.id), primary_key=True)
+    evento_id = db.Column ( db.Integer, db.ForeignKey(evento.id), primary_key=True)
+    usuario_id = db.Column ( db.Integer, db.ForeignKey(usuario.id), primary_key=True)
+    cantidad_Aportada = db.Column ( db.Integer,nullable=False)
+
+
+    def __repr__(self):
+        return "<Participante %r>" % self.cantidad_Aportada
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "cantidad aportada": self.cantidad_Aportada,
+            "id de evento": self.evento_id,
+            "id de usuario": self.usuario_id,
+            "id de item": self.item_id,
+        }
